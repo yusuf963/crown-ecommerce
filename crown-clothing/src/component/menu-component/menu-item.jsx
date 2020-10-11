7e05c1dd60bdffb37525b270a290d031a7035498
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-12 00:53:34 +0100

    adding style to menu component

--- crown-clothing/src/component/menu-component/menu-item.jsx
+++ crown-clothing/src/component/menu-component/menu-item.jsx
@@ -3,15 +3,16 @@ import './menu-item.scss'
 
 
 const MenuItem = ({title, imageUrl, size})=>(
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} 
-        className={`${size} menu-item`}>
-            <div className='content'>
+    <div className={`${size} menu-item`}>
+        <div
+            className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`}}/>
+        <div className='content'>
                 <h1 className='title'>{title}</h1>
                 <span className='subtitle'> SHOP NOW</span>
-             </div>
-         </div>   
+        </div>
+    </div>   
 )
 
 export default MenuItem;

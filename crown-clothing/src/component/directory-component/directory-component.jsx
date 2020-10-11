fb57e2a12f1c0475bf8a3061c3d14d4653b5ebd4
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-11 23:59:20 +0100

    adding attribute to directory componet

--- crown-clothing/src/component/directory-component/directory-component.jsx
+++ crown-clothing/src/component/directory-component/directory-component.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
 import MenuItem from '../menu-component/menu-item';
+import './directory-componet.scss';
 
 class Directory extends React.Component{
     constructor(){
         super();
         this.state={
-            section=[
+            section:[
                 {
-                    title: 'hats',
+                    title: 'Hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
                     linkUrl: 'shop/hats'
                   },
                   {
-                    title: 'jackets',
+                    title: 'Jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
                     linkUrl: 'shop/jackets'
                   },
                   {
-                    title: 'sneakers',
+                    title: 'Sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     id: 3,
                     linkUrl: 'shop/sneakers'
                   },
                   {
-                    title: 'womens',
+                    title: 'Womens',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                   },
                   {
-                    title: 'mens',
+                    title: 'Mens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
@@ -42,11 +43,13 @@ class Directory extends React.Component{
         }
     }
     render(){
+        return(
         <div className='directory-menu'>
-        {this.state.section.map(({title, imageUrl, id})=>(
-            <MenuItem key={id} title={title} image={imageUrl}/>
+        {this.state.section.map(({title, imageUrl, id, size})=>(
+            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
         </div>
+        )
     }
 }
 

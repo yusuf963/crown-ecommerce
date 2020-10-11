d9e97f6ae6e0b2fcb3c7997023d1e9d02ad077b8
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-12 00:03:48 +0100

    adding images attribute

--- crown-clothing/src/component/menu-component/menu-item.jsx
+++ crown-clothing/src/component/menu-component/menu-item.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import './menu-item.scss'
 
 
-const MenuItem = ({title})=>(
-    <div className='menu-item'>
+const MenuItem = ({title, imageUrl, size})=>(
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }} 
+        className={`${size} menu-item`}>
             <div className='content'>
                 <h1 className='title'>{title}</h1>
                 <span className='subtitle'> SHOP NOW</span>
@@ -11,4 +14,4 @@ const MenuItem = ({title})=>(
          </div>   
 )
 
-export default MenuItem
+export default MenuItem;

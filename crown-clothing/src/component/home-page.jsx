1dda19db54b2734041495f3febcca3666c3dea48
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-12 00:01:29 +0100

    link menu item component to directory component and added style

--- crown-clothing/src/component/home-page.jsx
+++ crown-clothing/src/component/home-page.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
+import Directory from './directory-component/directory-component'
 import './homepage.styles.scss'
 
 
 const HomePage =()=>(
     <div className='homepage'>
-        <div className='directory-menu'>
-         <div className='menu-item'>
-            <div className='content'>
-                <h1 className='title'>HAT</h1>
-                <span className='subtitle'> SHOP NOW</span>
-             </div>
-         </div>   
-         <div className='menu-item'>
-            <div className='content'>
-                <h1 className='title'>JACKET</h1>
-                <span className='subtitle'> SHOP NOW</span>
-             </div>
-         </div>    
-         <div className='menu-item'>
-            <div className='content'>
-                <h1 className='title'>SNEAKERS</h1>
-                <span className='subtitle'> SHOP NOW</span>
-             </div>
-         </div>    
-         <div className='menu-item'>
-            <div className='content'>
-                <h1 className='title'>WOMENS</h1>
-                <span className='subtitle'> SHOP NOW</span>
-             </div>
-         </div>    
-         <div className='menu-item'>
-            <div className='content'>
-                <h1 className='title'>MANS</h1>
-                <span className='subtitle'> SHOP NOW</span>
-             </div>
-         </div>     
-        </div>
+    <Directory/>
     </div>
 )
 

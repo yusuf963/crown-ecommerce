f634971162dddc8d9456f98a3434cf4fd40630c7
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-11 21:34:47 +0100

    installing sass and tags for homepage

---
+++ crown-clothing/src/component/home-page.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+const HomePage =()=>(
+    <div className='homepage'>
+        <div className='directory-menu'>
+         <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>HAT</h1>
+                <span className='subtitle'> SHOP NOW</span>
+             </div>
+         </div>   
+         <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>JACKET</h1>
+                <span className='subtitle'> SHOP NOW</span>
+             </div>
+         </div>    
+         <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>SNEAKERS</h1>
+                <span className='subtitle'> SHOP NOW</span>
+             </div>
+         </div>    
+         <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>WOMENS</h1>
+                <span className='subtitle'> SHOP NOW</span>
+             </div>
+         </div>    
+         <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>MANS</h1>
+                <span className='subtitle'> SHOP NOW</span>
+             </div>
+         </div>     
+        </div>
+    </div>
+)
+
+export default HomePage;

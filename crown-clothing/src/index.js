bbd2b8b0f2977e89f9e84ee6d35637979a81dce4
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-12 22:46:29 +0100

    adding react-router-dom

--- crown-clothing/src/index.js
+++ crown-clothing/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import{BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
-    <App />,
+    <App />
+ ,
   document.getElementById('root')
 );
 

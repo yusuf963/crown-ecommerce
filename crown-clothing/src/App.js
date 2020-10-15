643fb6297f04b80d45a5f0f852fbd9c799201287
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-15 04:21:41 +0100

    adding shop preview components

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import HomePage from './component/home-page';
 import {Switch, Route, Link} from 'react-router-dom';
-
-
-const HatPage =()=>(
-  <div>
-    <Link to= '/'>Click here to see hats</Link>
-    <h1>Hats page</h1>
-  </div>
-)
- 
-const HatPageId =(props)=>(
- <div>
-    <button onClick={()=>props.history.push('/')}>Click here to got back to the home stor</button>
-   <h3>Hat id is {props.match.params.hatid}</h3>
- </div>
-)
+import ShopPage from './component/shop/shop.component.jsx'
 
 
 function App(){
@@ -23,7 +9,7 @@ function App(){
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop/hats' component={HatPage}/> 
+        <Route exact path='/shop/hats' component={ShopPage}/> 
       </Switch>
     </div>
   );

208f379abcc45a277c24c5da16c92a37995fdf24
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-15 04:22:25 +0100

    pulling data for shop

---
+++ crown-clothing/src/component/shop/shop.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SHOP_DATA from './shop.data'
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            collection : SHOP_DATA
+        }    
+        
+     }
+     render(){
+         return<div>navigation working successfully</div>
+     }
+    }
+export default ShopPage;

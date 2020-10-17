2c3827e575f5f6384e50cc406f9ae80a26df0524
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-17 01:24:58 +0100

    creating shop component

--- crown-clothing/src/component/shop/shop.component.jsx
+++ crown-clothing/src/component/shop/shop.component.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import SHOP_DATA from './shop.data'
+import CollectionPreview from '../preview-collection/preview-collection-component'
 
 class ShopPage extends React.Component{
     constructor(props){
         super(props)
         this.state ={
-            collection : SHOP_DATA
-        }    
-        
+            collections : SHOP_DATA
+        };   
      }
      render(){
-         return<div>navigation working successfully</div>
+         const {collections}= this.state;
+         return(
+             <div className='shop-page'>
+          {
+              collections.map(({id, ...otherCollectionProps})=>(
+                  <CollectionPreview key={id} {...otherCollectionProps}/>
+              ))}
+             </div>
+             )
      }
-    }
+     }
+
+    
 export default ShopPage;

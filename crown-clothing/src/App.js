965d97993091c2bbf4b448bb75886b93351c4065
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-17 01:59:05 +0100

    adding style to the App.css

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import HomePage from './component/home-page';
-import {Switch, Route, Link} from 'react-router-dom';
-import ShopPage from './component/shop/shop.component.jsx'
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css'
+
+import HomePage from './component/home-page';
+import ShopPage from './component/shop/shop.component.jsx'
+import Header from './component/header/hearder-cmponent'
+
 
 
 function App(){
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop/hats' component={ShopPage}/> 

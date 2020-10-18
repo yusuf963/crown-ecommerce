e815caeb72a8336ff341555f4c99940f830b515b
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-19 00:00:52 +0100

    adding firebase auth to the web app

--- crown-clothing/src/component/firebase/firebase.utils.js
+++ crown-clothing/src/component/firebase/firebase.utils.js
@@ -18,7 +18,7 @@ const config = {
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
-  const provider = new firebase.auth.GithubAuthProvider();
+  const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({prompt: 'select_account'});
   export const signInWithGoogle = ()=> auth.signInWithPopup(provider);
 

20136264520c02c423ea81167e5b362bf338a167
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-19 00:12:48 +0100

    adding extra div holding all the buttons

--- crown-clothing/src/component/sign-in-up/sign-in.jsx
+++ crown-clothing/src/component/sign-in-up/sign-in.jsx
@@ -48,8 +48,10 @@ class SignIn extends React.Component{
                  label='password'
                  handleChange={this.handleChange}
                  required />
-                 <CustomButton type='submit'>Sign In </CustomButton>
-                 <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+                 <div className='buttons'>
+                    <CustomButton type='submit'>Sign In </CustomButton>
+                    <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+                 </div>
              </form>
             </div>
             )

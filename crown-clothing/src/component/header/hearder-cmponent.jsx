b39306a95d0e6a32cc1d393d809d081563e62c3b
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-19 00:02:48 +0100

    src/component/header/hearder-cmponent.jsx

--- crown-clothing/src/component/header/hearder-cmponent.jsx
+++ crown-clothing/src/component/header/hearder-cmponent.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import './header-component.scss';
+
+import {auth} from '../firebase/firebase.utils'
+
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
-const Header =()=>(
+import './header-component.scss';
+
+const Header =({ currentUser })=>(
     <div  className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -15,6 +19,12 @@ const Header =()=>(
             <Link className='option' to= '/shop'>
                 CONTACT
             </Link>
+            {
+                currentUser?
+                <div className ='option' onClick={()=> auth.signOut()}> SIGN OUT</div>
+                :
+                <Link className= 'option' to ='/signin'>SIGN IN</Link>
+            }
         </div>
     </div>
 )

574ceb47f4452c8f35d8d89980399f38e6c91865
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-18 01:02:38 +0100

    ct

---
+++ crown-clothing/src/component/sign-in-up/sign-in-up-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-in-up.scss';
+
+import SignIn from './sign-in';
+
+const SignInAndSignUpPage = ()=>(
+    <div className='sign-in-and-sign-up'><SignIn/></div>
+)
+
+
+
+export default SignInAndSignUpPage;
---
+++ crown-clothing/src/component/sign-in-up/sign-in.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './sign-in.scss';
+
+
+class SignIn extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            email: '',
+            password: ''
+        }
+    }
+    handleSubmit = event =>{
+        event.preventDefault();
+        this.setState({email: '', password:''})
+    }
+    handleChange= event =>{
+        const {value, name}= event.target;
+        this.setState({[name]: value})
+    }
+        render(){
+          return(
+            <div className='sign-in'>
+             <h1>I already have an account</h1>
+             <span>Sign in with your email and password</span>
+
+             <form onSubmit={this.handleSubmit} >
+                 <input 
+                 name='email' 
+                 type='email' 
+                 value={this.state.email}
+                 onChange={this.handleChange}
+                  required />
+                 <label>Email</label> 
+                 <input 
+                 name='password' 
+                 type='password' 
+                 value={this.state.password} 
+                 required />
+                 <label>Email</label>
+                 <input type='submit' 
+                 value='submit'/>
+             </form>
+            </div>
+            )
+    }  
+}
+    
+
+
+export default SignIn;

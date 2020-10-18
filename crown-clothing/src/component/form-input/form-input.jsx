2759bebac7ffe6fe751b5d824cdce7c76badf9a9
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-18 02:46:35 +0100

    adding style to form component

--- crown-clothing/src/component/form-input/form-input.jsx
+++ crown-clothing/src/component/form-input/form-input.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './form-input-style.scss';
 
+
 const FormInput =({ handleChange, label, ...otherProps})=>(
     <div className='group'>
      <input className='form-input' onChange={handleChange} {...otherProps} />
      {
          label ?
-        ( <label className={`${props.value.length ? 'shrink' : ''} form-input-label`}>
+        ( <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
          </label>)
          : null
      }

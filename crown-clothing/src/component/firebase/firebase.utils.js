2f26489c2d30073dfc43f197c162e05c0b4e56f3
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-20 03:09:55 +0100

    adding user data storing

--- crown-clothing/src/component/firebase/firebase.utils.js
+++ crown-clothing/src/component/firebase/firebase.utils.js
@@ -13,6 +13,29 @@ const config = {
     measurementId: "G-3D52BRPF1X"
   };
 
+  export const createUserProfileDocument = async (userAuth, addditionalData)=>{
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot =  await userRef.get();
+    if(!snapShot.exists){
+      const {displayName, email}= userAuth;
+      const createAt = new Date();
+
+      try{
+        await userRef.set({
+          displayName,
+          email,
+          createAt,
+          ...addditionalData
+        })
+      }catch(error){
+          console.log('error creating user', error.message)
+      }
+    }
+      return userRef;
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();

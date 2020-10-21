045329c6113ac0e626f5fb278307ba083e46806e
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-22 00:15:02 +0100

    completing sign in component

--- crown-clothing/src/component/sign-in-up/sign-in-up-component.jsx
+++ crown-clothing/src/component/sign-in-up/sign-in-up-component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+
 import './sign-in-up.scss';
 
 import SignIn from './sign-in';
+import SignUp  from '../signUp-component/signUp'
 
 const SignInAndSignUpPage = ()=>(
-    <div className='sign-in-and-sign-up'><SignIn/></div>
+    <div className='sign-in-and-sign-up'>
+
+    <SignIn/>
+    <SignUp/>
+    </div>
 )
 
 

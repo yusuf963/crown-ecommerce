25b102e7014520f651f29ae74fbcb31f404cbe3f
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-22 00:14:16 +0100

    adding divs for form input component

--- crown-clothing/src/component/form-input/form-input.jsx
+++ crown-clothing/src/component/form-input/form-input.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import './form-input-style.scss';
 
 
-const FormInput =({ handleChange, label, ...otherProps})=>(
+const FormInput =({ handleChange, label, ...otherProps })=>(
     <div className='group'>
      <input className='form-input' onChange={handleChange} {...otherProps} />
-     {
-         label ?
-        ( <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
-         </label>)
-         : null
-     }
+     {label ?(
+          <label 
+            className={`${
+                otherProps.value ? 'shrink' : ''
+                } form-input-label`}
+            >
+     {label}
+         </label>
+         ): null}
     </div>
 )
 
-
+// missing value.length as it was causing error
 export default FormInput;

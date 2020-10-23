85625de8c35f4b6b5f720007cc9618e07ab14518
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-23 04:09:49 +0100

    working on user using reduce

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux'
 
 import './App.css'
@@ -44,13 +44,15 @@ componentDidMount(){
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/shop/hats' component={ShopPage}/> 
-          <Route exact path='/signin' component={SignInAndSignUpPage}/>
+          <Route exact path='/signin' render={()=> this.props.currentUser? (<Redirect to ='/'/>): (<SignInAndSignUpPage/>)}/>
         </Switch>
       </div>
     );
   }
 };
- 
+ const mapStateToProps =({user})=>({
+   currentUser: user.currentUser
+ })
 const mapDispatchToProps = dispatch =>({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
---
+++ crown-clothing/src/redux/user/user.type.js
@@ -0,0 +1,5 @@
+export const UserActionType ={
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+
+}
+export default UserActionType

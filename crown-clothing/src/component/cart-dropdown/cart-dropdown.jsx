4298d64e65d84cedeec4c40183ee3838cedf82da
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-25 15:59:49 +0000

    creating cart component

---
+++ crown-clothing/src/component/cart-dropdown/cart-dropdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CustomButton from '../custom-button/custom-button';
+import './cart-dropdown.scss';
+
+
+const CartDropDown =()=> (
+    <div className='cart-dropdown'>
+      <div className='cart-item'/>
+      <CustomButton>GO TO CHERCKOUT</CustomButton>
+    </div>
+);
+
+export default CartDropDown;

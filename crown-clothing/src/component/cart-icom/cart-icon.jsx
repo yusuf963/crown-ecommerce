7de5e9b57281e793a902ba2a016cebb42530633f
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-11-01 16:41:46 +0000

    adding dropdown menue

--- crown-clothing/src/component/cart-icom/cart-icon.jsx
+++ crown-clothing/src/component/cart-icom/cart-icon.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {  connect } from 'react-redux';
+import { toggleCartHidden} from '../../redux/cart/cart.action'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.scss';
 
-const CartIcon =()=>(
-    <div className='cart-icon'>
+const CartIcon =({toggleCartHidden})=>(
+    <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>0</span>
     </div>
-)
+);
+const mapDispatchProps = dispatch =>({
+    toggleCartHidden: ()=> dispatch(toggleCartHidden())
+});
 
-export default CartIcon;
+export default connect(
+    null, 
+    mapDispatchProps
+) 
+(CartIcon);

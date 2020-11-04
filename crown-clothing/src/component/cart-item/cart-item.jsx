0266ba4ec28bdbec5d8e26e37350e0bdf2c62d76
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-11-04 20:39:36 +0000

    making folder and file for cart component with style file

---
+++ crown-clothing/src/component/cart-item/cart-item.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './cart-item.scss';
+
+const CartItem =({ item: {imageUrl, price, name, quantity} })=>(
+    <div className= 'cart-name'>
+        <img src={imageUrl} alt='item'/>
+        <div className='item-details'>
+            <span className='name'>{name}</span>
+            <span className='price'> {quantity} * ${price}</span>
+        </div>
+    </div>
+);
+
+export default CartItem

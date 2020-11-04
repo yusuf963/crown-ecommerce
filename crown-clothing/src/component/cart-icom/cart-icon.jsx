0a69cf24657e7847df547752afac7dee88909381
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-11-04 20:37:18 +0000

    styling cart component

--- crown-clothing/src/component/cart-icom/cart-icon.jsx
+++ crown-clothing/src/component/cart-icom/cart-icon.jsx
@@ -4,18 +4,24 @@ import { toggleCartHidden} from '../../redux/cart/cart.action'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.scss';
 
-const CartIcon =({toggleCartHidden})=>(
+const CartIcon =({toggleCartHidden, itemCount})=>(
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 const mapDispatchProps = dispatch =>({
     toggleCartHidden: ()=> dispatch(toggleCartHidden())
 });
+const mapStateToProps = ({cart : {cartItems} })=>({
+    itemCount: cartItems.reduce(
+        (accumalatedQuantity, cartItem)=> accumalatedQuantity + cartItem.quantity, 
+        0
+        )
+});
 
 export default connect(
-    null, 
-    mapDispatchProps
+    null,
+     mapDispatchProps
 ) 
 (CartIcon);

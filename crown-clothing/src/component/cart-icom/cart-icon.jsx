6d94e89cddf9959af054a164c847ce001c838402
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-11-05 21:42:19 +0000

    replacying the state by reselect

--- crown-clothing/src/component/cart-icom/cart-icon.jsx
+++ crown-clothing/src/component/cart-icom/cart-icon.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {  connect } from 'react-redux';
-import { toggleCartHidden} from '../../redux/cart/cart.action'
+import { createStructuredSelector} from 'reselect';
+
+import { toggleCartHidden} from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.scss';
 
@@ -13,15 +16,12 @@ const CartIcon =({toggleCartHidden, itemCount})=>(
 const mapDispatchProps = dispatch =>({
     toggleCartHidden: ()=> dispatch(toggleCartHidden())
 });
-const mapStateToProps = ({cart : {cartItems} })=>({
-    itemCount: cartItems.reduce(
-        (accumalatedQuantity, cartItem)=> accumalatedQuantity + cartItem.quantity, 
-        0
-        )
+const mapStateToProps = createStructuredSelector ({
+    itemCount: selectCartItemsCount
 });
 
 export default connect(
-    null,
+    mapStateToProps,
      mapDispatchProps
 ) 
 (CartIcon);

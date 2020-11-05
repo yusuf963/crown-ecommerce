bdf22eef2ebaf68bafd877bfb1069e5dc72b05e7
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-11-05 23:25:55 +0000

    adding items to checkout page

--- crown-clothing/src/component/check-out-page/checkout.jsx
+++ crown-clothing/src/component/check-out-page/checkout.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector';
+import CheckoutItem from '../checkout-item/ckeckout-item';
 import './checkout.style.scss';
 import FormInput from '../form-input/form-input';
 
@@ -31,10 +32,9 @@ const CheckoutPage=({ cartItems, total})=>(
             </div>
         </div>
         {
-            cartItems.map(cartItem=>
-                cartItem.name
-            )
-        }
+            cartItems.map(cartItem=> 
+            <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+            )}
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>

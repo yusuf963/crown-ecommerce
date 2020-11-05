1053234be62c4994ebcae551149d1932322856b9
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-11-05 21:50:00 +0000

    creatimg span message when cart is empty

--- crown-clothing/src/component/cart-dropdown/cart-dropdown.jsx
+++ crown-clothing/src/component/cart-dropdown/cart-dropdown.jsx
@@ -14,9 +14,12 @@ const CartDropDown =({cartItems})=> (
     <div className='cart-dropdown'>
       <div className='cart-items'>
         {
+          cartItems.length ?
           cartItems.map(cartItem => (
             <CartItem key={cartItem.id} item={cartItem}/>
           ))
+          : 
+          <span className='empty-message'>Your cart is empty</span>
         }
       </div>
       <CustomButton>GO TO CHERCKOUT</CustomButton>

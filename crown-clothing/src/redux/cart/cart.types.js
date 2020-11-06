dc809348490656f5bad6fa23a29c63d7e4fc34a6
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-11-06 13:29:55 +0000

    destructing the state in cart tpes file

--- crown-clothing/src/redux/cart/cart.types.js
+++ crown-clothing/src/redux/cart/cart.types.js
@@ -1,5 +1,7 @@
 const CartActionTypes ={
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-    ADD_ITEM: 'ADD_Item'
+    ADD_ITEM: 'ADD_Item',
+    REMOVE_ITEM: 'REMOVE_ITEM',
+    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
 };
 export default CartActionTypes;
